d02d634dc8f47ef945e4bfd01584a18f13ec66e9
ektx <530675800@qq.com>
2018-03-30 14:44:39 +0800

    添加点击选中效果

--- js/cmap.js
+++ js/cmap.js
@@ -30,6 +30,8 @@ class CMap {
 		this.animatePause = true
 		// 当前鼠标移入区索引
 		this.mouseMoveIndex = -1
+		// 选择区域
+		this.holdBlocks = []
 	}
 
 	/**
@@ -422,10 +424,37 @@ class CMap {
 		let mapX = 0
 		let mapY = 0
 		let oldArr = []
+		let _blocks = this.options.map.blocks
+		let _selectedMode = _blocks.selectedMode
 		let mouseMove = {
 			hold: false,
 			x: 0,
-			y: 0
+			y: 0,
+			status: false // 记录是否有移动
+		}
+
+		let checkInMap = (x, y, callback) => {
+			const pixel = this.hitCtx.getImageData(x, y, 1, 1).data
+			const color = `rgb(${pixel[0]},${pixel[1]},${pixel[2]})`
+			const shape = this.colorsHash[color]
+
+			if (shape) callback(shape)
+		}
+
+		let reSetCanvas = (x, y) => {
+			mapX = x - mouseMove.x + this.mapTranslateX
+			mapY = y - mouseMove.y + this.mapTranslateY
+
+			this.clearCanvasCtx()
+			this.ctx.translate(mapX, mapY)
+			this.hitCtx.translate(mapX, mapY)
+			this.drawAllBoundary()
+			this.ctx.setTransform(1, 0, 0, 1, 0, 0)
+			this.hitCtx.setTransform(1, 0, 0, 1, 0, 0)
+		}
+
+		let inHoldBlocks = index => {
+			return this.holdBlocks.includes(this.mouseMoveIndex)
 		}
 
 		this.ele.addEventListener('mousemove', evt => {
@@ -434,38 +463,33 @@ class CMap {
 			
 			// 按住地图时
 			if (evt.buttons && mouseMove.hold) {
-				mapX = x - mouseMove.x + this.mapTranslateX
-				mapY = y - mouseMove.y + this.mapTranslateY
+				// 地图拖动
+				mouseMove.status = true
 
-				this.clearCanvasCtx()
-				this.ctx.translate(mapX, mapY)
-				this.hitCtx.translate(mapX, mapY)
-				this.drawAllBoundary()
-				this.ctx.setTransform(1, 0, 0, 1, 0, 0)
-				this.hitCtx.setTransform(1, 0, 0, 1, 0, 0)
+				reSetCanvas(x, y)
 			} else {
-				const pixel = this.hitCtx.getImageData(x, y, 1, 1).data
-				const color = `rgb(${pixel[0]},${pixel[1]},${pixel[2]})`
-				const shape = this.colorsHash[color]
-				const _blocks = this.options.map.blocks
 				const _callback = this.options.callback
 
 				// 恢复之前鼠标移入对象效果
 				if (this.mouseMoveIndex > -1) {
-					_blocks.data[this.mouseMoveIndex].style.fillStyle = _blocks.style.fillStyle
+					_blocks.data[this.mouseMoveIndex].style.fillStyle = inHoldBlocks(this.mouseMoveIndex) ? _blocks.style.holdColor : _blocks.style.fillStyle
 					this.mouseMoveIndex = -1
+
+					this.clearCanvasCtx()
+					this.drawAllBoundary()
 				}
 
-				if (shape) {
+				checkInMap(x, y, shape => {
 					_blocks.data[shape.index].style.fillStyle = _blocks.style.hoverColor
 					this.mouseMoveIndex = shape.index
 
-					if (_callback && _callback.hasOwnProperty('mousemove'))
+					if (_callback && _callback.hasOwnProperty('mousemove')) {
 						_callback.mousemove(evt, shape)
-				}
-
-				this.clearCanvasCtx()
-				this.drawAllBoundary()
+					}
+	
+					this.clearCanvasCtx()
+					this.drawAllBoundary()
+				})
 			}
 
 		})
@@ -476,27 +500,56 @@ class CMap {
 			mouseMove.x = evt.offsetX * this.DPI
 			mouseMove.y = evt.offsetY * this.DPI
 			
+			oldArr = [this.mapTranslateX, this.mapTranslateY]
 			if (this.scaleEvtStatus) {
-				oldArr = [this.mapTranslateX, this.mapTranslateY]
 				this.mapTranslateX = 0
 				this.mapTranslateY = 0
 			}
 		})
 
 		this.ele.addEventListener('mouseup', evt => {
-			// this.animatePause = false
-			this.mapTranslateX = mapX //this.DPI
-			this.mapTranslateY = mapY //this.DPI
-
-			// 在缩放过的情况下
-			if (this.scaleEvtStatus) {
-				this.scaleEvtStatus = false
-				this.mouseMoveStatus = oldArr // 坐标发生变化前值
-			}
+			mouseMove.hold = false
 			
-			this.scaleMap(this.mapScale)
-			this.animate()
-			console.log('End', mapX, mapY, this.mapTranslateX)
+			if (mouseMove.status) {
+				// this.animatePause = false
+				mouseMove.status = false
+
+				this.mapTranslateX = mapX //this.DPI
+				this.mapTranslateY = mapY //this.DPI
+
+				// 在缩放过的情况下
+				if (this.scaleEvtStatus) {
+					this.scaleEvtStatus = false
+					this.mouseMoveStatus = oldArr // 坐标发生变化前值
+				}
+				
+				this.scaleMap(this.mapScale)
+				this.animate()
+			} else {
+				let x = evt.offsetX * this.DPI
+				let y = evt.offsetY * this.DPI
+
+				checkInMap(x, y, shape => {
+					if (inHoldBlocks(shape.index)) {
+						let _index = this.holdBlocks.indexOf(shape.index)
+						
+						this.holdBlocks.splice(_index, 1)
+					} else {
+						if (_selectedMode === 'multiple') {
+							this.holdBlocks.push(shape.index)
+						} else if (_selectedMode === 'single') {
+							this.holdBlocks = [shape.index]
+						}
+					}
+
+					this.holdBlocks.forEach(val => {
+						_blocks.data[val].style.fillStyle = _blocks.style.holdColor
+					})
+				})
+				
+				this.mouseMoveStatus = oldArr 
+				this.scaleMap(this.mapScale)
+			}
 		})
 	}
 
